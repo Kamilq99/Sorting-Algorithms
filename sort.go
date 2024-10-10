a31db5d4d9071674e8e9548fb3cbd3c26f48c91c
Kamilq99 <83961352+Kamilq99@users.noreply.github.com>
2024-10-10 13:30:56 +0200

    Bubble Sort Function

--- sort.go
+++ sort.go
@@ -4,11 +4,22 @@ import (
 	"fmt"
 )
 
+func BubbleSort(arr []int) {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	fmt.Printf("Sorted array by Bubble Sort: %v", arr)
+}
+
 func main() {
 
-	arr := [10]int{34, 12, 5, 99, 23, 47, 8, 64, 28, 1}
+	arr := []int{34, 12, 5, 99, 23, 47, 8, 64, 28, 1}
 
-	fmt.Pritnf("Unsorted array: %v", arr)
+	fmt.Printf("Unsorted array: %v", arr)
 
 	var choice int
 
@@ -29,16 +40,19 @@ func main() {
 		BubbleSort(arr)
 	case 2:
 		SelectionSort(arr)
-	case 3:
-		InsertionSort(arr)
-	case 4:
-		QuickSort(arr, 0, len(arr)-1)
-	case 5:
-		MergeSort(arr, 0, len(arr)-1)
-	case 6:
-		BucketSort(arr)
-	case 7:
-		HeapSort(arr)
+	/*
+		case 3:
+			InsertionSort(arr)
+		case 4:
+			QuickSort(arr, 0, len(arr)-1)
+		case 5:
+			MergeSort(arr, 0, len(arr)-1)
+		case 6:
+			BucketSort(arr)
+		case 7:
+			HeapSort(arr)
+
+	*/
 	default:
 		fmt.Println("Invalid choice")
 	}
